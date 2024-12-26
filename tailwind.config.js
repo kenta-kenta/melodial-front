b08dee550eefc8c163d4e88da06e8f204b7b4e5f
kenta-kenta <kenken22352235@gmail.com>
2024-12-26 21:44:58 +0900

    tailwindとpacage.jsonを編集

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Noto Sans JP', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
